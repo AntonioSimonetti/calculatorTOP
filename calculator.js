2fa810f780c00c05b52911736fc16b54171b9db3
AntonioSimonetti <antonosimonettiantonio@gmail.com>
2022-05-27 17:16:29 +0200

    first html structure written, main css done and some basic js added but to be modified

--- calculator.js
+++ calculator.js
@@ -1,1 +1,30 @@
-console.log(`hi`);
+const add = function(a, b) {
+    return a + b;
+  };
+  
+  const subtract = function(a, b) {
+    return a - b;
+  };
+  
+  const sum = function(array) {
+    return array.reduce((total, current) => total + current, 0);
+  };
+  
+  const multiply = function(array) {
+      return array.length
+        ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+        : 0;
+  };
+
+
+const domEle = function () {
+    let take = document.getElementById("btn").textContent;
+    console.log(take);
+    let display = document.getElementById("displayCalculator");
+    display.innerHTML = take;
+}
+
+  
+  /* const power = function(a, b) {
+    return Math.pow(a, b);
+  }; */
